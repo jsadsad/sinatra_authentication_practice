c5b0c9d12b227ad77c2533d7bd02b0af1c34afc0
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-05-13 15:40:14 -0700

    login views

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sinatra_authentication_practice
+
+Made with <3 by J.S.
---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "auh_demo"
+  end
+
+end
